cd80c4dbad745a88a3773d62d5e1ebc18f0a7251
davithovhann1syan <davit05hovhannisyan@gmail.com>
2023-02-08 21:40:12 +0400

    updated welcome page

--- app/src/main/java/com/example/myapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/myapplication/WelcomeActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +10,19 @@ import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    TextView next, login, register;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        TextView next = findViewById(R.id.nextPage);
+        next = findViewById(R.id.nextPage);
+        register = findViewById(R.id.go_to_register_page);
+        login = findViewById(R.id.go_to_login_page);
+
+
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,5 +30,22 @@ public class WelcomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+            }
+        });
+
+
+
     }
 }
